27e5d127950052ee71e8d913bdf89932ef20ee76
zcqznj <291414600@qq.com>
2017-06-17 22:10:26 +0800

    新增后天更新，图标更换，修复沉浸状态栏

--- app/src/main/java/com/myweather/android/WeatherActivity.java
+++ app/src/main/java/com/myweather/android/WeatherActivity.java
@@ -1,6 +1,9 @@
 package com.myweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
@@ -19,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.myweather.android.gson.Forecast;
 import com.myweather.android.gson.Weather;
+import com.myweather.android.service.AutoUpdateService;
 import com.myweather.android.util.HttpUtil;
 import com.myweather.android.util.Utility;
 
@@ -64,13 +68,14 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (Build.VERSION.SDK_INT >= 21) {
-//            View decordView = getWindow().getDecorView();
-//            decordView.setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//            );
-//            getWindow().setStatusBarColor(Color.TRANSPARENT);
-//        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decordView = getWindow().getDecorView();
+            decordView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
         // 初始化各控件
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
@@ -207,6 +212,13 @@ public class WeatherActivity extends AppCompatActivity {
      * 处理并展示Weather实体类中的数据。
      */
     private void showWeatherInfo(Weather weather) {
+        if(weather!=null&&"ok".equals(weather.status)){
+            Intent intent=new Intent(this, AutoUpdateService.class);
+            startService(intent);
+
+        }else {
+            Toast.makeText(this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+        }
         String cityName = weather.basic.cityName;
         String updateTime = weather.basic.update.updateTime.split(" ")[1];
         String degree = weather.now.temperature + "℃";
